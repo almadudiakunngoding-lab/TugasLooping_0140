da8486c7d26c81a446c901c809539e2b26fe7d53
almaudud <almadudiakunngoding@gmail.com>
2026-04-01 17:32:16 +0700

    menambabhkan while loop untuk membuat deret

--- tugaslooping1.cpp
+++ tugaslooping1.cpp
@@ -32,5 +32,12 @@ bool apakahPrima(int n) {
     int a = 0, b = 1;
     if (n == a || n == b) return true;
     
-   
+    int c = a + b;
+    while (c <= n) {
+        if (c == n) return true;
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return false;
 }   
