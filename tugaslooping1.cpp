a9f53e965ea7c70a7ae139c70194561e74180e5a
almaudud <almadudiakunngoding@gmail.com>
2026-04-01 17:19:14 +0700

    menambahkan menu pegecekan bilangan prima dan finonacci

--- tugaslooping1.cpp
+++ tugaslooping1.cpp
@@ -7,4 +7,9 @@ int angka;
 int pilihan;
 
 void tampilkanmenu(){
-   
+    cout<<"===MENU PENGECEKKAN ANGKA===";
+    cout<<"1. mengecek bilangan prima";
+    cout<<"2. mengecek bilangan Fibonacci";
+    cout<<"3. keluar dari menu";
+    cin>> pilihan;
+}
