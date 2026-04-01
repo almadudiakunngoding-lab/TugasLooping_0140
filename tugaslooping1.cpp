ffdbff09682e8e28819e036d50259cc760679a46
almaudud <almadudiakunngoding@gmail.com>
2026-04-01 17:29:33 +0700

    menambahkan while loop

--- tugaslooping1.cpp
+++ tugaslooping1.cpp
@@ -22,4 +22,8 @@ void inputData() {
 bool apakahPrima(int n) {
     if (n <= 1) return false;
     int i = 2;
-    
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
